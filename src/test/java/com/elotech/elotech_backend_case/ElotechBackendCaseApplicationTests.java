0f21a2bad148aa03a31adf9ae9914bd573b5b07b
FellipeOliveiraBartalini <fellipe.oliveira.work@gmail.com>
2025-10-29 12:49:09 -0300

    chores

---
+++ src/test/java/com/elotech/elotech_backend_case/ElotechBackendCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elotech.elotech_backend_case;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElotechBackendCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
